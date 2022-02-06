666d18a934221b222b4fa269dcf5d74febf9c58b
Shrankhla Srivastava <shrankhla.srivastava@gmail.com>
2022-02-06 14:27:37 +0530

    Time: 2 ms (99.35%), Space: 43.4 MB (58.85%) - LeetHub

---
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] asteroidCollision(int[] arr) {
+        int n = arr.length;
+        LinkedList<Integer> st = new LinkedList<>();
+        for(int ele : arr){
+            if(ele>0){
+                st.addFirst(ele);
+                continue;
+            }
+            while(st.size() != 0 && st.getFirst() > 0 && st.getFirst() < -ele)
+                st.removeFirst();
+            if(st.size() != 0 && st.getFirst() == -ele)
+                st.removeFirst();
+            else if(st.size() == 0 || st.getFirst() < 0)
+                st.addFirst(ele);
+            else{
+                
+            }
+        }
+        int[] ans = new int[st.size()];
+        for(int i=ans.length-1; i>=0; i--){
+            ans[i] = st.removeFirst();
+        }
+        return ans;
+    }
+}
