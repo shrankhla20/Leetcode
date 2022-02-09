87a72b7f5eadc9110e4858fe19fd1ab127118a4c
Shrankhla Srivastava <shrankhla.srivastava@gmail.com>
2022-02-09 17:25:46 +0530

    Time: 68 ms (48.70%), Space: 121.6 MB (5.01%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int[] nsol = NSOL(heights);
+        int[] nsor = NSOR(heights);
+        
+        int maxArea = 0;
+        for(int i =0; i<heights.length ; i++){
+            maxArea = Math.max(maxArea, heights[i]*(nsor[i]-nsol[i]-1));
+            
+        }
+        return maxArea;
+    }
+    public static int[] NSOR(int[] arr){
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,n);
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+        
+        for(int i =0; i<n; i++){
+            while(st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
+                ans[st.removeFirst()] = i;
+            st.addFirst(i);
+        }
+        return ans;
+    }
+     public static int[] NSOL(int[] arr){
+        int n = arr.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,-1);
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
+        
+        for(int i =n-1; i>=0 ; i--){
+            while(st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
+                ans[st.removeFirst()] = i;
+            st.addFirst(i);
+        }
+        return ans;
+    }
+}
