ff1ee6ffac00ee5ee3ad03a1d5f227af4ca2a8d1
Shrankhla Srivastava <shrankhla.srivastava@gmail.com>
2022-02-16 18:52:48 +0530

    Time: 44 ms (62.73%), Space: 95.7 MB (10.94%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,41 +1,22 @@
 class Solution {
     public int largestRectangleArea(int[] heights) { //7n
-        int[] nsol = NSOL(heights); //3n
-        int[] nsor = NSOR(heights); //3n
+        int n = heights.length, maxArea = 0;
+        LinkedList<Integer> st = new LinkedList<>();
+        st.addFirst(-1);
         
-        int maxArea = 0;
-        for(int i =0; i<heights.length ; i++){
-            maxArea = Math.max(maxArea, heights[i]*(nsor[i]-nsol[i]-1));
-            
+        for(int i=0; i<n; i++){
+            while(st.getFirst() != -1 && heights[st.getFirst()] >= heights[i]){
+                int h = heights[st.removeFirst()];
+                int w = i - st.getFirst() -1;
+                maxArea = Math.max(maxArea, h*w);
+            }
+            st.addFirst(i);
+        }
+        while(st.getFirst() != -1){
+            int h = heights[st.removeFirst()];
+            int w = n - st.getFirst() -1;
+            maxArea = Math.max(maxArea, h*w);
         }
         return maxArea;
-    }
-    public static int[] NSOR(int[] arr){
-        int n = arr.length;
-        int[] ans = new int[n];
-        Arrays.fill(ans,n);
-        LinkedList<Integer> st = new LinkedList<>();
-        st.addFirst(-1);
-        
-        for(int i =0; i<n; i++){
-            while(st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
-                ans[st.removeFirst()] = i;
-            st.addFirst(i);
-        }
-        return ans;
-    }
-     public static int[] NSOL(int[] arr){
-        int n = arr.length;
-        int[] ans = new int[n];
-        Arrays.fill(ans,-1);
-        LinkedList<Integer> st = new LinkedList<>();
-        st.addFirst(-1);
-        
-        for(int i =n-1; i>=0 ; i--){
-            while(st.getFirst() != -1 && arr[st.getFirst()] > arr[i])
-                ans[st.removeFirst()] = i;
-            st.addFirst(i);
-        }
-        return ans;
     }
 }
