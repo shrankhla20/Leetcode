6cc08c5fe27c31d38b17902c42bf6fb4f74cca18
Shrankhla Srivastava <shrankhla.srivastava@gmail.com>
2022-04-19 23:16:39 +0530

    Time: 1 ms (94.87%), Space: 40.7 MB (86.36%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,21 +1,27 @@
 class Solution {
-    public int numDecodings(String s, int idx, int[] dp){
-        if(idx == s.length())
-            return dp[idx] = 1;
+    public int numDecodings(String s, int IDX, int[] dp){
+        for(int idx = s.length(); idx >= 0; idx--){
+            if(idx == s.length()){
+               dp[idx] = 1;
+                continue;
+            }
         
-        if(dp[idx] != -1)return dp[idx];
+            char ch = s.charAt(idx);
+            if(ch == '0'){
+                dp[idx] = 0;
+                continue;
+            }
+            int count = dp[idx+1];
         
-        char ch = s.charAt(idx);
-        if(ch == '0')return dp[idx] = 0;
-        int count = numDecodings(s,idx+1,dp);
-        
-        if(idx < s.length() -1){
-            char ch1 = s.charAt(idx +1);
-            int num = (ch - '0')*10 + (ch1-'0');
-            if(num <= 26)
-                count += numDecodings(s,idx+2,dp);
+            if(idx < s.length() -1){
+                char ch1 = s.charAt(idx +1);
+                int num = (ch - '0')*10 + (ch1-'0');
+                if(num <= 26)
+                    count += dp[idx+2];
+            }
+            dp[idx] = count;
         }
-        return dp[idx] = count;
+        return dp[IDX];
     }
     
     public int numDecodings(String s) {
