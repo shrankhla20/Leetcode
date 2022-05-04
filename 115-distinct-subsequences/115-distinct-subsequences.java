25dd59b8b8ba80fa8deaeaf7719c7df8f153941c
Shrankhla Srivastava <shrankhla.srivastava@gmail.com>
2022-05-04 18:39:15 +0530

    Time: 29 ms (38.72%), Space: 50 MB (54.01%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.java
+++ 115-distinct-subsequences/115-distinct-subsequences.java
@@ -6,14 +6,24 @@ class Solution {
         
         return numDistinct(s,t,n,m,dp);
     }
-    public int numDistinct(String s, String t, int n, int m, int[][] dp) {
-        if(m == 0)return dp[n][m] = 1;
-        if(n<m)return dp[n][m] = 0;
-        if(dp[n][m] != -1)return dp[n][m];
-        int a = numDistinct(s,t,n-1,m-1,dp);
-        int b = numDistinct(s,t,n-1,m,dp);
-        if(s.charAt(n-1) == t.charAt(m-1))return dp[n][m] = a+b;
-        else return dp[n][m] = b;
+    public int numDistinct(String s, String t, int N, int M, int[][] dp) {
+        for(int n =0; n<= N; n++){
+            for(int m = 0; m<= M; m++){
+                if(m == 0){
+                    dp[n][m] = 1;
+                    continue;
+                }
+                if(n<m){
+                    dp[n][m] = 0;
+                    continue;
+                }
+                int a = dp[n-1][m-1];//numDistinct(s,t,n-1,m-1,dp);
+                int b = dp[n-1][m]; //numDistinct(s,t,n-1,m,dp);
+                if(s.charAt(n-1) == t.charAt(m-1))dp[n][m] = a+b;
+                else dp[n][m] = b;
         
+            }
+        }
+        return dp[N][M];
     }
 }
